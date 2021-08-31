4f7da7df6d8520dd87dcb37d4bb806264fbadf41
Prachi Sahani <prachirsahani97@gmail.com>
2021-09-01 00:41:21 +0530

    integrated api and improved ui

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var btnTranslate = document.querySelector('#btn-translate');
-var txtInput = document.querySelector('#txt-input');
-var output = document.querySelector('#output');
-// console.log(btnTranslate); 
-function clickEventhandler(){
-    output.innerHTML = 'hello ' + txtInput.value
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+var result;
+var url = "https://api.funtranslations.com/translate/minion.json";
+// var url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+// console.log(btnTranslate);
+function clickEventhandler() {
+  fetch(getTranslationUrl(txtInput.value))
+    .then((response) =>  response.json())
+    .then((json) => {
+      if (json.success) {
+        output.innerHTML = json.contents.translated;
+      }
+      else{
+          throw json.error
+      }
+    })
+    .catch(errorHandler);
 }
-btnTranslate.addEventListener('click', clickEventhandler)
 
+function getTranslationUrl(text){
+      return `${url}?text=${text}`
+}
 
+function errorHandler(error) {
+  console.log("error occured: ", error);
+  alert(error.message);
+}
+btnTranslate.addEventListener("click", clickEventhandler);
