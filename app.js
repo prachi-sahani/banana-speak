235169caaa4f56fbd1d682682960b6ce5806bedd
Prachi Sahani <prachirsahani97@gmail.com>
2021-09-01 21:18:05 +0530

    minor change in functionality

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ var url = "https://api.funtranslations.com/translate/minion.json";
 // var url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
 // console.log(btnTranslate);
 function clickEventhandler() {
+  btnTranslate.innerHTML = 'Translating...';
+  output.innerHTML = '';
   fetch(getTranslationUrl(txtInput.value))
     .then((response) =>  response.json())
     .then((json) => {
+      btnTranslate.innerHTML = 'Translate'
+
       if (json.success) {
         output.innerHTML = json.contents.translated;
+
       }
       else{
           throw json.error
